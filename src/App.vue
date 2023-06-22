8f66e8f850e7b0408a9f2c4f1a7b08b91364ed6b
Dongwoo Kim <superjoy0502@gmail.com>
2023-06-22 00:02:20 -0600

    stuff

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,49 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="navbar">
+    <nav class="navbar navbar-expand-lg navbar-fixed-top">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Karos</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Experiences</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Links
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="http://discord.karos.me/">Discord</a></li>
+                <li><a class="dropdown-item" href="http://fb.karos.me/">Facebook</a></li>
+                <li><a class="dropdown-item" href="http://github.karos.me/">GitHub</a></li>
+                <li><a class="dropdown-item" href="http://ig.karos.me/">Instagram</a></li>
+                <li><a class="dropdown-item" href="http://yt.karos.me/">YouTube</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+  <div>
+    <h1 class="mx-auto">{{ name }}</h1>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      msg: 'Karos',
+      name: 'Joy'
+    }
   }
 }
-</style>
+</script>
